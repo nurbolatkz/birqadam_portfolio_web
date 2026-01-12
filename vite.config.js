f5a1ac7f08ebf956aaaca31e330df969be04d22f
nurbolatkz <nurbolat.duisenbek001@gmail.com>
2026-01-13 00:52:32 +0500

    fix dokcer compose file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: false,
+    allowedHosts: ['birqadam.kz', 'localhost'],
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+    hmr: {
+      overlay: false
+    }
+  }
 })
